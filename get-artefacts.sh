6ad64dc5543891ed63795c3e1abd6bb0135f296b
Morgan2304 <morgan.sailly@sfr.fr>
2025-11-05 10:29:01 +0000

    fin de seance

---
+++ get-artefacts.sh
@@ -0,0 +1,5 @@
+curl \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer <YOUR-TOKEN>" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/Morgan2304/graphes/actions/artifacts
